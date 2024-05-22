1c0882ef3fb239ce1363b383102be4f944fd75b8
Alastors <78517796+Alastors@users.noreply.github.com>
2024-05-22 00:41:28 -0400

    I always forget spotless

--- src/main/java/common/Recipes.java
+++ src/main/java/common/Recipes.java
@@ -1,11 +1,5 @@
 package common;
 
-import static gregtech.api.enums.Mods.ThaumicTinkerer;
-
-import java.util.HashMap;
-
-import net.minecraft.item.ItemStack;
-
 import common.recipeLoaders.AlloySmelter;
 import common.recipeLoaders.Assembler;
 import common.recipeLoaders.AssemblyLine;
@@ -16,11 +10,6 @@ import common.recipeLoaders.Mixer;
 import common.recipeLoaders.ResearchableAssemblyLine;
 import common.recipeLoaders.Unpackager;
 
-import cpw.mods.fml.common.registry.GameRegistry;
-import gregtech.api.enums.Materials;
-import gregtech.api.enums.OrePrefixes;
-import gregtech.api.util.GT_ModHandler;
-import gregtech.api.util.GT_OreDictUnificator;
 import kekztech.KekzCore;
 
 public class Recipes {
